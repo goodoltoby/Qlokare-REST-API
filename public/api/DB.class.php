ac05185d2f841514fc1312015feeb46bac388b2c
goodoltoby <tob.jan87@gmail.com>
2016-03-07 14:19:06 +0100

    added more basic functions

---
+++ public/api/DB.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class DB{
+
+  private static $instance; 
+
+  public static function getInstance(){
+    if(!self::$instance){ # Om vi redan har något i $instance i klassen (self::)
+      # Skapa då ett mysqli-objekt med en kopplaing till vår databas och lagra den i $instance
+      require_once('db.conf.php');
+      self::$instance = new mysqli(CONF_DB_SERVER,CONF_DB_USERNAME,CONF_DB_PASSWORD,CONF_DB_DATABASE);
+      self::$instance->query("SET NAMES 'utf8'");
+      return self::$instance;
+    }else{ # Om vi inte har något i $instance
+      return self::$instance;
+    }
+  }
+
+  private function __construct(){}
+  private function __clone(){}
+}
---
+++ public/api/index.php
@@ -0,0 +1,59 @@
+<?php
+header('Content-Type: text/json');
+
+
+function __autoload($class){
+  require_once($class.".class.php");
+}
+
+$url_parts = getUrlParts($_GET);
+
+$method = $_SERVER['REQUEST_METHOD']; # innehåller ex: POST, PUT, DELETE, GET
+$resource = $url_parts[0];
+$data = getHTTPData($method);
+
+$allowed_resources = ['user', 'help', 'helplist'];
+
+if(in_array($resource, $allowed_resources)){
+  require_once($resource.".class.php");
+  $id = (isset($url_parts[1])) ? $url_parts[1] : NULL;
+  $obj = new $resource($id);
+  $obj->$method($data); 
+}else{
+  header("HTTP/1.1 404 Not Found");
+}
+
+
+
+
+function getHTTPData($method){
+  switch($method){
+    case 'GET':
+      $data = $_GET;
+      break;
+    case 'POST':
+      $data = $_POST;
+      break;
+    case 'PUT':
+    case 'DELETE':
+      parse_str(file_get_contents('php://input', false , null, -1 , $_SERVER['CONTENT_LENGTH'] ), $data);
+      break;
+  }
+  return $data;
+}
+
+
+
+function getUrlParts($get){
+  $get_params = array_keys($get);
+  
+  $url = $get_params[0];
+  $url_parts = explode("/",$url);
+  foreach($url_parts as $k => $v){
+    if($v) $array[] = $v; # om det finns ett innehåll på platsen vi är på just nu, spara det i $array
+  }
+
+  $url_parts = $array;
+
+  return $url_parts; 
+}
