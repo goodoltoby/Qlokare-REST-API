6017f3fc983b5b1f6fe0292f93af01c4cf88606a
goodoltoby <tob.jan87@gmail.com>
2016-03-10 15:27:50 +0100

    fail

--- public/api/db.conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('CONF_DB_SERVER', 'qlokarerestapi-214562.mysql.binero.se');
-define('CONF_DB_USERNAME', '214562_oe94369');
-define('CONF_DB_PASSWORD', 'Hejaaik123');
-define('CONF_DB_DATABASE', '214562-qlokarerestapi');
